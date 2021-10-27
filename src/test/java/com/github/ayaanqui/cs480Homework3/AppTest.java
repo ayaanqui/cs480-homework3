834bf8d08bdeb3829e31307a3688c6c1f1824ac1
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-26 19:31:21 -0500

    new project structure

---
+++ src/test/java/com/github/ayaanqui/cs480Homework3/AppTest.java
@@ -0,0 +1,11 @@
+package com.github.ayaanqui.cs480Homework3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void myTest() {
+        Assert.assertEquals(1, 1);
+    }
+}
