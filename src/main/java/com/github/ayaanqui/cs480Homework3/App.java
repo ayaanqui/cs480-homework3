5a84e511d2f859b1781f7b3f9753fd38b414adb3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-28 20:51:03 -0500

    create tables

--- src/main/java/com/github/ayaanqui/cs480Homework3/App.java
+++ src/main/java/com/github/ayaanqui/cs480Homework3/App.java
@@ -3,6 +3,7 @@ package com.github.ayaanqui.cs480Homework3;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     public static Connection establishConnect(String username, String password, String dbName) {
@@ -14,19 +15,48 @@ public class App {
         }
     }
 
+    /**
+     * Creates employee and department tables. And creates relations between the two
+     * tables
+     * 
+     * @param conn Active MySQL connection
+     */
+    public static void createTables(final Connection conn) {
+        try (Statement statement = conn.createStatement()) {
+            String createEmployeeTable = "CREATE TABLE IF NOT EXISTS employee" + "(ename VARCHAR(255) NOT NULL,"
+                    + " deptName VARCHAR(255) NOT NULL" + " salary DECIMAL(13,2) NOT NULL"
+                    + " city VARCHAR(255) NOT NULL," + " PRIMARY KEY (ename))";
+            String createDepartmentTable = "CREATE TABLE IF NOT EXISTS department" + "(deptName VARCHAR(255) NOT NULL,"
+                    + " mname VARCHAR(255) NOT NULL," + " PRIMARY KEY (deptName))";
+
+            statement.executeUpdate(createEmployeeTable);
+            statement.executeUpdate(createDepartmentTable);
+        } catch (SQLException e) {
+            System.err.println(e.toString());
+        }
+    }
+
     public static void main(String[] args) {
-        String filename = "db_config1.json";
+        String filename = "db_config.json";
         DB.DbConfig dbConfig = new DB(filename).getConfig();
         if (dbConfig == null) {
             System.err.println("File not found or json is invalid. Make sure to read README.md file");
         }
 
-        Connection c = establishConnect(dbConfig.username, dbConfig.password, dbConfig.dbName);
-        if (c == null) {
+        final Connection conn = establishConnect(dbConfig.username, dbConfig.password, dbConfig.dbName);
+        if (conn == null) {
             System.err.printf(
                     "Could not establish connection with DB. Make sure %s has the username, password, and database name",
                     filename);
+            return;
         }
-        System.out.println(c.toString());
+
+        // Creates employee and department tables
+        createTables(conn);
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
